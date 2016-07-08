1ca4715048a5b8c66c4b78f5254932ec5adfd435
jetsonhacks <jim@jetsonhacks.com>
2016-07-08 09:20:05 -0700

    Initial Commit

---
+++ installLibfreenect2.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+git clone https://github.com/OpenKinect/libfreenect2.git
+cd libfreenect2
+cd depends; ./download_debs_trusty.sh
+sudo apt-get install build-essential cmake pkg-config -y
+sudo dpkg -i debs/libusb*deb
+sudo dpkg -i debs/libglfw3*deb; sudo apt-get install -f; sudo apt-get install libgl1-mesa-dri-lts-vivid -y
+# Make and Install libfreenect2
+cd ..
+mkdir build && cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=$HOME/freenect2
+make
+make install
