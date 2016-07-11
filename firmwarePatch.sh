cf01d994552f24b32270ec581aeb98d48d9f70b9
Jetsonhacks <jim@jetsonhacks.com>
2016-07-10 21:02:54 -0700

    Add reboot message to firmware patch script

--- firmwarePatch.sh
+++ firmwarePatch.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 sudo cp firmwarePatch/xusb_sil_rel_fw-test /lib/firmware/tegra21x_xusb_firmware
+/bin/echo -e "\e[1;32mFinished. Please Reboot for firmware changes to take effect.\e[0m"
 
